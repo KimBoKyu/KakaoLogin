ddd13ae51d9853e3c6207b5738db36ca0a074688
KimBoKyu <sa5469@naver.com>
2019-05-14 19:44:10 +0900

    sds

--- app/src/main/java/application/mobile/kakaologin/FirstActivity.java
+++ app/src/main/java/application/mobile/kakaologin/FirstActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class FirstActivity extends Activity {
-    ///asdasdad
+    ///asdasdadasd
     Handler handler = new Handler();
     Runnable r = new Runnable() {
         @Override
